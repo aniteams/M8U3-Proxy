c85c390b259fa9d0e7cae2cfad556fd416ed025f
EternalAnime <eternalmelodymagic@gmail.com>
2025-07-19 19:26:31 +0530

    Update m3u8-proxy.ts

--- src/controllers/m3u8-proxy.ts
+++ src/controllers/m3u8-proxy.ts
@@ -10,14 +10,37 @@ export const m3u8Proxy = async (req: Request, res: Response) => {
     const isStaticFiles = allowedExtensions.some(ext => url.endsWith(ext));
     const baseUrl = url.replace(/[^/]+$/, "");
 
-    const response = await axios.get(url, {
-      responseType: 'stream',
-      headers: {
-        Accept: "*/*",
-        Referer: "https://megaplay.buzz/",
-        Origin: "https://megaplay.buzz"
+    // List of Referer/Origin combinations to try
+    const headerOptions = [
+      { Referer: "https://megaplay.buzz/", Origin: "https://megaplay.buzz" },
+      { Referer: "https://vidwish.live/", Origin: "https://vidwish.live" },
+      { Referer: "https://kwik.cx/", Origin: "https://kwik.cx" },
+      { Referer: "https://tubeplx.viddsn.cfd", Origin: "https://tubeplx.viddsn.cfd" }
+    ];
+
+    let response;
+    let lastError;
+
+    for (const headers of headerOptions) {
+      try {
+        response = await axios.get(url, {
+          responseType: 'stream',
+          headers: {
+            Accept: "*/*",
+            Referer: headers.Referer,
+            Origin: headers.Origin
+          }
+        });
+        break; // If successful, break out of the loop
+      } catch (err: any) {
+        lastError = err;
+        if (err.response?.status !== 403 && err.response?.status !== 401) {
+          break; // If error is not 403/401, break the loop (e.g., 404)
+        }
       }
-    });
+    }
+
+    if (!response) throw lastError;
 
     const headers = { ...response.headers };
     if (!isStaticFiles) delete headers['content-length'];
@@ -25,14 +48,10 @@ export const m3u8Proxy = async (req: Request, res: Response) => {
     res.cacheControl = { maxAge: headers['cache-control'] };
     headers["Access-Control-Allow-Origin"] = "*";
     headers["Access-Control-Allow-Headers"] = "*";
-    headers["Access-Control-Allow-Methods"] = "*"
+    headers["Access-Control-Allow-Methods"] = "*";
     res.set(headers);
 
-    if (isStaticFiles) {
-      return response.data.pipe(res);
-    }
-
-    if (!url.endsWith(".m3u8")) {
+    if (isStaticFiles || !url.endsWith(".m3u8")) {
       return response.data.pipe(res);
     }
 
@@ -42,4 +61,4 @@ export const m3u8Proxy = async (req: Request, res: Response) => {
     console.log(error.message);
     res.status(500).send('Internal Server Error');
   }
-}
+};
